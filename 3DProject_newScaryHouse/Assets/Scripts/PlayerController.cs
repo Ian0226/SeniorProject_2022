9c817fc4f18d88a3466f3e1ebb59ddd8099ec216
Ian <asdasd15915943@gmail.com>
2022-09-19 01:05:06 +0800

    王羿鈞的熊熊初上傳

--- 3DProject_newScaryHouse/Assets/Scripts/PlayerController.cs
+++ 3DProject_newScaryHouse/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //移動速度
     [SerializeField]
     private float moveSpeed;
     public float MoveSpeed
@@ -11,6 +12,7 @@ public class PlayerController : MonoBehaviour
         get { return moveSpeed; }
         set { moveSpeed = value; }
     }
+    //滑鼠靈敏度
     [SerializeField]
     private float mouseSensitivity;
     public float MouseSensitivity
@@ -25,6 +27,7 @@ public class PlayerController : MonoBehaviour
     private Transform mainCameraTransform;
     [SerializeField]
     private float interactiveRange;
+    //玩家當前互動的物件
     private GameObject nowInteractiveObj;
     private CharacterController playerCtrl;
 
@@ -47,6 +50,7 @@ public class PlayerController : MonoBehaviour
         if (Physics.Raycast(ray, out hitObj, interactiveRange) )
         {
             hitObj.transform.SendMessage("HitByRaycast", gameObject, SendMessageOptions.DontRequireReceiver);
+            nowInteractiveObj = hitObj.transform.gameObject;
             //Debug.DrawLine(ray.origin, hitObj.point, Color.yellow);
             //print(hitObj.transform.name);
         }
